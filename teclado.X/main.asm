772fab4d42827dad8bb17a55671fc5890895b5f2
Danteps-4 <danteaugsburger4@gmail.com>
2025-06-14 10:05:04 -0300

    cuenta regresiva

--- teclado.X/main.asm
+++ teclado.X/main.asm
@@ -16,7 +16,11 @@ AUX_KEYNUM EQU 0x22
 SALVAW EQU 0x23
 SALVAS EQU 0x24
 ACTIVADA EQU 0x25
-
+ACTIVANDOSE EQU 0x26
+CUENTA_REGRESIVA EQU 0x27
+ 
+; Los contadores para el delay de 1 seg de la cuenta regresiva
+CONT1 EQU 0x30
 ;-------------------------------
 ; PROGRAMA PRINCIPAL
 ;-------------------------------
@@ -30,7 +34,7 @@ INICIO
     BANKSEL ANSELH
     CLRF ANSELH
     BANKSEL OPTION_REG
-    MOVLW b'01110111'	;Pullups PORTB=on, T0CS=T0CKI (pin para detener el timer), Prescaler 256
+    MOVLW b'01110111'	;Pullups PORTB=("0" es que estan activadas), T0CS=T0CKI (pin para detener el timer), Prescaler 256
     MOVWF OPTION_REG
     BANKSEL TRISB
     MOVLW b'11110000'
@@ -54,7 +58,7 @@ INICIO
     BCF STATUS,RP1
     BANKSEL TRISE
     CLRF TRISE
-    BANKSEL PORTA	    ; Habilito el PORTA como salidas para led activada, buzzer, etc
+    BANKSEL PORTA	    ; Habilito el PORTA como salidas para led, buzzer, etc
     CLRF PORTA
     BANKSEL ANSEL
     CLRF ANSEL
@@ -67,6 +71,8 @@ INICIO
     IORWF INTCON,F
     
     CLRF ACTIVADA
+    CLRF ACTIVANDOSE
+    CLRF CONT1 
     
 LOOP
     NOP
@@ -95,6 +101,19 @@ TABLA
     RETLW b'11111100'	;B
     RETLW b'10111001'	;C
     RETLW b'11011110'	;D
+    
+DISPLAY_7SEGMENTOS
+    ADDWF PCL,F
+    RETLW b'10111111'	;0
+    RETLW b'10000110'	;1
+    RETLW b'11011011'	;2
+    RETLW b'11001111'	;3
+    RETLW b'11100110'	;4
+    RETLW b'11101101'	;5
+    RETLW b'11111101'	;6
+    RETLW b'10000111'	;7
+    RETLW b'11111111'	;8
+    RETLW b'11100111'	;9
     
 ISR
     ; Guardo el contexto
@@ -190,6 +209,7 @@ SR_KEY
     BTFSC STATUS,Z
     GOTO CHEQUEO_ALARMA	    ; SI se presiono, voy a chequear la alarma
     RETURN		    ; NO se presiono, vuelvo
+    
 CHEQUEO_ALARMA
     MOVF ACTIVADA,W	    ; Chequeo si la alarma esta activada
     SUBLW d'1'		    ; Le resto 1 a ACTIVADA
@@ -199,9 +219,60 @@ CHEQUEO_ALARMA
     
 ACTIVAR_ALARMA
     MOVLW 0x01
-    MOVWF ACTIVADA	    ; Muevo un "1" a ACTIVADA para decir que la alarma esta activada (fase 1 de ACTIVADA - para realizar la cuenta regresiva mas adelante)
+    MOVWF ACTIVANDOSE	    ; Pongo un "1" en ACTIVANDOSE para decir que estoy en la cuenta regresiva
+    
+    MOVLW 0x09
+    MOVWF CUENTA_REGRESIVA  ; Cargo CUENTA_REGRESIVA con el numero 9 para hacer la cuenta regresiva
+ 
+OTRA_CUENTA_REGRESIVA
+    MOVF CUENTA_REGRESIVA,W
+    CALL DISPLAY_7SEGMENTOS
+    MOVWF PORTD		    ; Lo muestro en el display
+    
+    CALL DELAY_1SEG	    ; LLamo a un delay de 1 seg
+    
+    DECFSZ CUENTA_REGRESIVA,F
+    GOTO OTRA_CUENTA_REGRESIVA	; Si no es 0, vuelvo a contar
+    
+    BANKSEL PORTD	    ; Si SI es 0
+    CLRF PORTD		    ; Limpio el PORTD indicando que se activo
+    
+    CLRF ACTIVANDOSE	    ; Limpio la variable ACTIVANDOSE
+    
+    MOVLW 0x01
+    MOVWF ACTIVADA	    ; Muevo un "1" a ACTIVADA para decir que la alarma esta activada
     BANKSEL PORTA
     BSF PORTA,RA0	    ; Prendo el led conectado a RA0
+    RETURN
+
+DELAY_1SEG
+    ; Configuraciones del delay
+    MOVLW d'20'
+    MOVWF CONT1
+    BANKSEL OPTION_REG
+    BCF OPTION_REG,T0CS
+    BANKSEL INTCON
+    BCF INTCON,T0IF
+    BSF INTCON,T0IE
+    
+ESPERA_EXTERIOR    
+    BANKSEL TMR0
+    MOVLW d'60'
+    MOVWF TMR0
+ESPERA
+    BANKSEL INTCON
+    BTFSS INTCON,T0IF
+    GOTO ESPERA
+    BCF INTCON,T0IF
+    DECFSZ CONT1,F
+    GOTO ESPERA_EXTERIOR
+    
+    BANKSEL OPTION_REG
+    BSF OPTION_REG,T0CS
+    BANKSEL INTCON
+    BCF INTCON,T0IF
+    BCF INTCON,T0IE
+    
     RETURN
     
 DESACTIVAR_ALARMA
