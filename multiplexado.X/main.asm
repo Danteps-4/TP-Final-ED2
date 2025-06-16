0a8cdcab68712b8ebfc628fdf26abef88d42ee83
Danteps-4 <danteaugsburger4@gmail.com>
2025-06-16 12:19:46 -0300

    multiplexado prueba funcional

--- multiplexado.X/main.asm
+++ multiplexado.X/main.asm
@@ -1,5 +1,5 @@
 ;-------------------------------
-; CONFIGURACIÓN
+; CONFIGURACI?N
 ;-------------------------------
     LIST P=16F887
     #include "p16f887.inc"
@@ -10,9 +10,12 @@
 ;-------------------------------
 ; VARIABLES
 ;-------------------------------
-CONT1 EQU 0x20
-CONT2 EQU 0x21
- 
+DIGI1 EQU 0x20
+DIGI2 EQU 0x21
+R1 EQU 0x30
+R2 EQU 0x31
+
+
 ;-------------------------------
 ; PROGRAMA PRINCIPAL
 ;-------------------------------
@@ -24,21 +27,71 @@ INICIO
     BANKSEL PORTD
     CLRF PORTD
     BANKSEL TRISD
-    CLRF TRISD		    ; Seteo PORTD todo como salidas para el display
+    CLRF TRISD
     BANKSEL PORTE
-    CLRF PORTE
+    CLRF PORTB
     BANKSEL ANSEL
     CLRF ANSEL
     BANKSEL TRISE
-    CLRF TRISE		    ; Seteo PORTE todo como salidas para el multiplexado
+    CLRF TRISE
     
-LOOP
-    BANKSEL PORTD
-    MOVLW b'11111111'
-    MOVWF PORTD
+    BANKSEL DIGI1
+    MOVLW d'2'
+    MOVWF DIGI1
+    MOVLW d'4'
+    MOVWF DIGI2
+    
+TODOS_DIG
+    MOVLW DIGI1
+    MOVWF FSR
+    MOVLW b'00000001'
     BANKSEL PORTE
-    BSF PORTE,0		    ; Habilito el display
-    GOTO LOOP
+    MOVWF PORTE
+OTRO_DIG
+    MOVF INDF,W
+    CALL DISPLAY_7SEGMENTOS
+    BANKSEL PORTD
+    MOVWF PORTD
+    CALL RETARDO_10MS
+    BCF STATUS,C
+    BANKSEL PORTE
+    RLF PORTE,F
+    INCF FSR,F
+    BTFSS PORTE,2
+    GOTO OTRO_DIG
+    GOTO TODOS_DIG
+    
 
+RETARDO_10MS
+    MOVLW   d'255'
+    MOVWF   R1         ; R1 = ciclo interior
+OUTER_LOOP
+    MOVLW   d'4'
+    MOVWF   R2         ; R2 = ciclo exterior
+INNER_LOOP
+    NOP                ; 1 ciclo
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    DECFSZ  R2,F       ; 1 ciclo si no es cero, 2 si es cero
+    GOTO    INNER_LOOP ; 2 ciclos
+    DECFSZ  R1,F
+    GOTO    OUTER_LOOP
+    RETURN
+
+DISPLAY_7SEGMENTOS
+    ADDWF PCL,F
+    RETLW b'10111111'	;0
+    RETLW b'10000110'	;1
+    RETLW b'11011011'	;2
+    RETLW b'11001111'	;3
+    RETLW b'11100110'	;4
+    RETLW b'11101101'	;5
+    RETLW b'11111101'	;6
+    RETLW b'10000111'	;7
+    RETLW b'11111111'	;8
+    RETLW b'11100111'	;9
+ 
     END
-
